8e285b49f567412c1804b8487fa439898352da06
Rajat <rajat1790@gmail.com>
2015-09-23 11:36:28 -0500

    hw 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,61 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.uniq.pluck(:rating)
+    @var = false
+    # Get the remembered settings
+    if (params[:ratings] == nil and params[:id] == nil and
+              (session[:ratings] != nil or session[:id] != nil))
+      if (params[:id] == nil and session[:id] != nil)
+        params[:id] = session[:id]
+      elsif(params[:ratings] == nil and session[:ratings] != nil)
+	params[:ratings] = session[:ratings]
+      end
+      flash.keep
+      redirect_to movies_path(:id => params[:id], :ratings => params[:ratings])
+    else
+      
+      @selected_ratings = @all_ratings
+      if(params[:ratings] == nil)
+	params[:ratings] = session[:ratings]
+	if(session[:ratings])
+          @selected_ratings = session[:ratings].keys
+	  @var = true
+	end
+      else
+	@selected_ratings = params[:ratings].keys
+      end
+      if(params[:id] == nil)
+	params[:id] = session[:id]
+      end
+      if(@var)
+	flash.keep
+	redirect_to movies_path(:id => params[:id], :ratings => params[:ratings])
+      end
+      session[:id] = params[:id]
+      session[:ratings] = params[:ratings]
+      if (params[:id] == "title_header") # Sort by titles
+	@title_header = "hilite"
+        if (params[:ratings]) # filter ratings
+          @movies = Movie.where(rating: @selected_ratings).order(:title)
+        else
+          @movies = Movie.order(:title)
+        end
+      elsif (params[:id] == "release_date_header") # Sort by release_date
+	@release_date = "hilite"
+        if (params[:ratings]) # filter ratings
+          @movies = Movie.where(rating: @selected_ratings).order(:release_date)
+        else
+          @movies = Movie.order(:release_date)
+        end
+      elsif (params[:id] == nil)
+        if (params[:ratings] or session[:ratings]) # filter ratings
+          @movies = Movie.where(rating: @selected_ratings)
+        else
+          @movies = Movie.all
+        end
+      end
+    end
   end
 
   def new
